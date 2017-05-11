e1abc2bf2b5d5290f6c77f47fdf5ea7980fa704b
karol17 <karol.mazur17@hotmail.com>
2017-05-11 23:02:34 +0200

    test

--- WebApplication1/WebApplication1/Global.asax.cs
+++ WebApplication1/WebApplication1/Global.asax.cs
@@ -1,5 +1,4 @@
-﻿using juve.App_Start;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,13 +6,14 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace juve
+namespace WebApplication1
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
---
+++ juve/juve/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace juve.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/reset.css",
+                      "~/Content/Style.css"));
+        }
+    }
+}
