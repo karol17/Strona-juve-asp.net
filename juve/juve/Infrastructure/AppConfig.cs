d21cc8a0a073465914ce61f81901b68139937c37
karol17 <karol.mazur17@hotmail.com>
2017-06-28 20:17:38 +0200

    Dodanie helpera do dynamicznego umieszczania zdjęć

---
+++ juve/juve/Infrastructure/AppConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace juve.Infrastructure
+{
+    public class AppConfig
+    {
+        private static string _obrazkiFolderWzgledny = ConfigurationManager.AppSettings["ObrazkiFolder"];
+
+        public static string ObrazkiFolderWzgledny
+        {
+            get { return _obrazkiFolderWzgledny; }
+        }
+    }
+}
